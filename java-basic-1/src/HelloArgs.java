bd36dca6ccc84fdbe6baf8d97b5c2a821e49ef0c
yuyadaruma24 <aimansoo0224@gamil.com>
2024-04-19 12:52:42 +0900

    hello変更

--- java-basic-1/src/HelloArgs.java
+++ java-basic-1/src/HelloArgs.java
@@ -1,6 +1,6 @@
 public class HelloArgs {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Worldddd!");
         System.out.println("args[0]"+args[0]);
         System.out.println("args[1]"+args[1]);
         System.out.println("args[2]"+args[2]);
