61bdc608b24d99f9d6d5c99fc7d96216049a5409
yuyadaruma24 <aimansoo0224@gamil.com>
2024-04-23 14:19:07 +0900

    変更

--- java-basic-1/src/NetproLabMember.java
+++ java-basic-1/src/NetproLabMember.java
@@ -4,14 +4,14 @@ public class NetproLabMember {
 
     public static void main(String[] args){
         Random random = new Random();
-        int[][] newMen = new int[15][3];
+        int[][] newMenber = new int[15][3];
         float rate = 1;
         for(int i = 0; i < 15; i ++){
-            newMen[i][0] = 110 + random.nextInt(20) - 10;
-            newMen[i][1] = newMen[i][0] * (20+i)/100;
-            newMen[i][2] = 10 + random.nextInt(6) - 3;
-            float manrate = newMen[i][1] / newMen[i][1];
-            rate = rate* newMen[i][2] / manrate;
+            newMenber[i][0] = 110 + random.nextInt(20) - 10; //入学生数
+            newMenber[i][1] = newMenber[i][0] * (20+i)/100;        //女子の割合
+            newMenber[i][2] = 10 + random.nextInt(6) - 3;    //岩井研の新メンバー
+            float manRate = newMenber[i][1] / newMenber[i][1]      //男の割合
+            rate = rate* newMenber[i][2] / manRate;
         }
         System.out.println(rate + "%");
     }
